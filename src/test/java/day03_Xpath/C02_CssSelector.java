7f1cadc6a6b3b17d9cbeafe6284bc7d5f994fe83
Ahmet Can Ates <omerzed54@gmail.com>
2022-12-20 23:57:46 +0300

    20/12/22

--- src/test/java/day03_Xpath/C02_CssSelector.java
+++ src/test/java/day03_Xpath/C02_CssSelector.java
@@ -19,7 +19,7 @@ public class C02_CssSelector {
     //8 Best Seller bolumunden ilk urunu tiklayin
     //9- Gift card details’den 25 $’i secin
     //10- Urun ucretinin 25$ oldugunu test edin
-    //10- Sayfayi kapatin
+    //11- Sayfayi kapatin
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -27,9 +27,13 @@ public class C02_CssSelector {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
+        //2- https://www.amazon.com/ adresine gidin
         driver.get("https://www.amazon.com/");
+        //3- Browseri tam sayfa yapin
         driver.manage().window().maximize();
+        //4- Sayfayi “refresh” yapin
         driver.navigate().refresh();
+        //5 Sayfa basliginin “Spend less” ifadesi icerdigini test edin
         String actualTitle = driver.getTitle();
         String expectedTitle = "Spend less";
         if(actualTitle.contains(expectedTitle)) {
@@ -37,18 +41,23 @@ public class C02_CssSelector {
         }else {
             System.out.println("TITLE TEST FAILED");
         }
+        //6 Gift Cards sekmesine basin
         driver.findElement(By.cssSelector("a[href='/gift-cards/b/?ie=UTF8&node=2238192011&ref_=nav_cs_gc']")).click();
+        //7 Birthday butonuna basin
         driver.findElement(By.cssSelector("img[src='https://images-na.ssl-images-amazon.com/images/G/01/gift-certificates/consumer/2021/GCLP/Support/2x/Occ_Birthday_346x130.png']")).click();
+        //8 Best Seller bolumunden ilk urunu tiklayin
         List<WebElement> bestSellerB = driver.findElements(By.cssSelector("img[alt='Amazon.com eGift Card']"));
         bestSellerB.get(0).click();
+        //9- Gift card details’den 25 $’i secin
         driver.findElement(By.cssSelector("button[value='25']")).click();
+        //10- Urun ucretinin 25$ oldugunu test edin
         WebElement urunUcreti = driver.findElement(By.cssSelector("span[id='gc-live-preview-amount']"));
         if(urunUcreti.getText().equals("$25.00")) {
             System.out.println("TEST PASSED");
         }else {
             System.out.println("TEST FAILED");
         }
-
+        //11- Sayfayi kapatin
         driver.close();
 
     }
